0789e2b8cf178dda7efb263474b43b81f241f8aa
Malcolm Peter <kraftycoder@gmail.com>
2018-06-12 17:50:28 +0300

    fixed undefined name error

--- app/models.py
+++ app/models.py
@@ -2,13 +2,13 @@ import psycopg2
 
 def connect():
     connect.conn = psycopg2.connect("dbname=d49pt4ur37g33c user=oqrnhavmylzeql password=290ca06f7d3667c7ebeb2d89f1ed502ce9db4ff7d91d2fd4269e92f7052a2283 host=ec2-54-225-241-25.compute-1.amazonaws.com port=5432")  # Connecting to the database
-    connect.cur = conn.cursor()  # Activate connection using the cursor
+    connect.cur = connect.conn.cursor()  # Activate connection using the cursor
 
 connect()
-cur = connect.cur
-print(cur)
 conn = connect.conn
 print(conn)
+cur = connect.cur
+print(cur)
 
 try:
     cur.execute('''CREATE TABLE IF NOT EXISTS articles(
