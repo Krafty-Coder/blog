7de74726ea1eae09dd8629c0d7a4f58e23c6f10a
Malcolm Peter <kraftycoder@gmail.com>
2018-08-10 09:02:58 +0300

    add js for the articles page

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -8,3 +8,20 @@ $(function() {
     }
   })
 })
+
+
+$(function(){
+  var btnSubmit = $('#share
+  btnSubmit.attr('disabled', 'disabled');
+  $('input[name=chosen]').change(function(e){
+    if($(this).val() == 'agree'){
+      btnSubmit.removeAttr('disabled');
+
+    }else{
+      btnSubmit.attr('disabled', 'disabled');
+
+    }
+
+  });
+
+});
