cd3a38c954201353ed9a8b7f671f233f6b732d54
Malcolm Peter <kraftycoder@gmail.com>
2018-09-17 17:36:14 +0300

    added global variables from class

--- app/models.py
+++ app/models.py
@@ -25,9 +25,9 @@ class Models():
 
 
     def connect(self):
-        conn = psycopg2.connect("dbname=d49pt4ur37g33c user=oqrnhavmylzeql password=290ca06f7d3667c7ebeb2d89f1ed502ce9db4ff7d91d2fd4269e92f7052a2283 host=ec2-54-225-241-25.compute-1.amazonaws.com port=5432")  # Connecting to the database
-        cur = conn.cursor()  # Activate connection using the cursor
-        return cur
+        self.connect.conn = psycopg2.connect("dbname=d49pt4ur37g33c user=oqrnhavmylzeql password=290ca06f7d3667c7ebeb2d89f1ed502ce9db4ff7d91d2fd4269e92f7052a2283 host=ec2-54-225-241-25.compute-1.amazonaws.com port=5432")  # Connecting to the database
+        self.Connect.cur = self.connect.conn.cursor()  # Activate connection using the cursor
+        return self.connect.cur
 
 
 models = Models()
