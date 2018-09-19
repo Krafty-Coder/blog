d393f1c432e116f3b849b2c4e316ec22e0c53650
Malcolm Peter <kraftycoder@gmail.com>
2018-09-19 17:47:02 +0300

    added global variables from function

--- app/models.py
+++ app/models.py
@@ -4,8 +4,11 @@ def connect():
     connect.conn = psycopg2.connect("dbname=d49pt4ur37g33c user=oqrnhavmylzeql password=290ca06f7d3667c7ebeb2d89f1ed502ce9db4ff7d91d2fd4269e92f7052a2283 host=ec2-54-225-241-25.compute-1.amazonaws.com port=5432")  # Connecting to the database
     connect.cur = conn.cursor()  # Activate connection using the cursor
 
+connect()
 cur = connect.cur
+print(cur)
 conn = connect.conn
+print(conn)
 
 try:
     cur.execute('''CREATE TABLE IF NOT EXISTS articles(
