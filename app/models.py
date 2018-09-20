94cbb98b62d32a341e05353431af9ec2d1183563
Malcolm Peter <kraftycoder@gmail.com>
2018-09-20 14:37:38 +0300

    commit after creation of tables

--- app/models.py
+++ app/models.py
@@ -3,21 +3,28 @@ import psycopg2
 conn = psycopg2.connect("dbname=d49pt4ur37g33c user=oqrnhavmylzeql password=290ca06f7d3667c7ebeb2d89f1ed502ce9db4ff7d91d2fd4269e92f7052a2283 host=ec2-54-225-241-25.compute-1.amazonaws.com port=5432")  # Connecting to the database
 cur = conn.cursor()  # Activate connection using the cursor
 
-
-cur.execute('''CREATE TABLE IF NOT EXISTS articles(
-    id serial PRIMARY KEY,
-    title varchar (50) NOT NULL,
-    author varchar (100) NOT NULL,
-    body text NOT NULL,
-    create_date timestamp default current_timestamp
+try:
+    cur.execute('''CREATE TABLE IF NOT EXISTS articles(
+        id serial PRIMARY KEY,
+        title varchar (50) NOT NULL,
+        author varchar (100) NOT NULL,
+        body text NOT NULL,
+        create_date timestamp default current_timestamp
     ) ''')
+    conn.commit()
+except:
+    print('Table already exists')
 
-cur.execute('''CREATE TABLE IF NOT EXISTS users(
-    id serial PRIMARY KEY,
-    name varchar (50) NOT NULL,
-    email varchar (100) NOT NULL,
-    username varchar (50) NOT NULL,
-    password varchar (100) NOT NULL,
-    join_date timestamp default current_timestamp
+try:
+    cur.execute('''CREATE TABLE IF NOT EXISTS users(
+        id serial PRIMARY KEY,
+        name varchar (50) NOT NULL,
+        email varchar (100) NOT NULL,
+        username varchar (50) NOT NULL,
+        password varchar (100) NOT NULL,
+        join_date timestamp default current_timestamp
     ) ''')
+    conn.commit()
+except:
+    print('Table already exists')
 
