22f58f9c8170dbf84e9d6b6cf10b87b6a96c2683
Malcolm Peter <kraftycoder@gmail.com>
2018-09-20 17:42:28 +0300

    change everything to not using oop

--- app/app.py
+++ app/app.py
@@ -1,18 +1,14 @@
 from functools import wraps
 from flask import (Flask, flash, redirect, render_template, request,
                    session, url_for)
+from app.models import connect
 import psycopg2
+from app.models import cur, conn
 from passlib.hash import sha256_crypt
 from wtforms import Form, PasswordField, StringField, TextAreaField, validators
 
-from app.models import Models
-
 app = Flask(__name__)
 
-connect = Models()
-connect = connect.connect()
-cur = connect.cur
-conn = connect.conn
 
 @app.route('/')
 def index():
--- app/models.py
+++ app/models.py
@@ -1,36 +1,34 @@
 import psycopg2
 
-class Models():
-    def init(self):
-        self.conn = psycopg2.connect("dbname=d49pt4ur37g33c user=oqrnhavmylzeql password=290ca06f7d3667c7ebeb2d89f1ed502ce9db4ff7d91d2fd4269e92f7052a2283 host=ec2-54-225-241-25.compute-1.amazonaws.com port=5432")  # Connecting to the database
-        self.cur = self.conn.cursor()  # Activate connection using the cursor
-        self.cur.execute('''CREATE TABLE IF NOT EXISTS articles(
-            id serial PRIMARY KEY,
-            title varchar (50) NOT NULL,
-            author varchar (100) NOT NULL,
-            body text NOT NULL,
-            create_date timestamp default current_timestamp
-        ) ''')
-        self.conn.commit()
+def connect():
+    connect.conn = psycopg2.connect("dbname=d49pt4ur37g33c user=oqrnhavmylzeql password=290ca06f7d3667c7ebeb2d89f1ed502ce9db4ff7d91d2fd4269e92f7052a2283 host=ec2-54-225-241-25.compute-1.amazonaws.com port=5432")  # Connecting to the database
+    connect.cur = conn.cursor()  # Activate connection using the cursor
 
-        self.cur.execute('''CREATE TABLE IF NOT EXISTS users(
-            id serial PRIMARY KEY,
-            name varchar (50) NOT NULL,
-            email varchar (100) NOT NULL,
-            username varchar (50) NOT NULL,
-            password varchar (100) NOT NULL,
-            join_date timestamp default current_timestamp
-        ) ''')
-        self.conn.commit()
+cur = connect.cur
+conn = connect.conn
 
+try:
+    cur.execute('''CREATE TABLE IF NOT EXISTS articles(
+        id serial PRIMARY KEY,
+        title varchar (50) NOT NULL,
+        author varchar (100) NOT NULL,
+        body text NOT NULL,
+        create_date timestamp default current_timestamp
+    ) ''')
+    conn.commit()
+except:
+    print('Table already exists')
 
-    def connect(self):
-        self.connect.conn = psycopg2.connect("dbname=d49pt4ur37g33c user=oqrnhavmylzeql password=290ca06f7d3667c7ebeb2d89f1ed502ce9db4ff7d91d2fd4269e92f7052a2283 host=ec2-54-225-241-25.compute-1.amazonaws.com port=5432")  # Connecting to the database
-        self.Connect.cur = self.connect.conn.cursor()  # Activate connection using the cursor
-        return self.connect.cur
-
-
-models = Models()
-models.connect()
-
+try:
+    cur.execute('''CREATE TABLE IF NOT EXISTS users(
+        id serial PRIMARY KEY,
+        name varchar (50) NOT NULL,
+        email varchar (100) NOT NULL,
+        username varchar (50) NOT NULL,
+        password varchar (100) NOT NULL,
+        join_date timestamp default current_timestamp
+    ) ''')
+    conn.commit()
+except:
+    print('Table already exists')
 
