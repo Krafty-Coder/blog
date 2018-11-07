5826ef1d08c9ba1d90847c506c1cecb9286562e1
Malcolm Peter <kraftycoder@gmail.com>
2018-11-07 17:51:49 +0300

    change access to os environs

--- app/dbInit.py
+++ app/dbInit.py
@@ -3,10 +3,11 @@ import os
 import psycopg2
 from passlib.hash import sha256_crypt
 
-dbname = os.environ.get('DB_NAME')
-dbuser = os.environ.get('DB_USER')
-dbpass = os.environ.get('DB_PASS')
-dbhost = os.environ.get('DB_HOST')
+dbname = os.environ.get('DBASE_NAME')
+dbuser = os.environ.get('DBASE_USER')
+dbpass = os.environ.get('DBASE_PASS')
+dbhost = os.environ.get('DBASE_HOST')
+dbport = os.environ.get('DBASE_PORT')
 
 queries = [
     '''
@@ -69,7 +70,7 @@ class Database():
         self.conn.close()
 
 
-db_url = "dbname={} user={} password={} host={} port=5432".format(dbname, dbuser, dbpass, dbhost)
+db_url = "dbname={} user={} password={} host={} port={}".format(dbname, dbuser, dbpass, dbhost, dbport)
 db = Database(db_url)
 db.create_tables()
 db.close_connection()
